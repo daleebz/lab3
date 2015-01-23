8c7be485f71b15794217b227c1b753500cf05df8
David Liberman <dsliberm@ucsd.edu>
2015-01-22 23:53:23 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,11 +8,30 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+function projectClick(e) {
+	console.log("Project clicked");
+	// prevent the page from reloading
+	e.preventDefault();
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	console.log("Number of matching items :" + 				 jumbotronHeader.length); 
+	jumbotronHeader.text(projectTitle);
+	
+	// in an event handler, $(this) refers to
+	// the object that triggered the event
+	$(this).css("background-color", "#7fff00");
+	var containingProject = $(this).closest(".project");
+	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+}
+
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Hello good sire"); 
+		$("#testjs").text("welcome sire");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
 }
